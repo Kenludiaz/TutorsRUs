33190ba7a6bd52dd34f3122a6278feba3b0e7321
Kenneth Lu Diaz <ludiazken@gmail.com>
2021-03-27 08:28:42 -0400

    Compile successfully

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-.COPY tutors(id, first_name,  last_name, email, phone, remote, rating, subject)
-FROM '/tutors.csv'
-DELIMITER ','
-CSV HEADER;
--- src/main/resources/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS tutors;
-CREATE TABLE tutors(id serial, first_name VARCHAR(255),  last_name VARCHAR(255), email VARCHAR(255), phone VARCHAR(14), remote BOOLEAN, rating FLOAT, subject VARCHAR(255));
